1fe28f65136ad10b6b5e36e057a036b84104725d
viakiba <892645423@qq.com>
2017-07-07 12:58:31 +0800

    lock 与 Synch

---
+++ LockVSSynchronized/src/com/vk/test_1/AppTest.java
@@ -0,0 +1,29 @@
+package com.vk.test_1;
+
+import java.util.concurrent.CyclicBarrier;
+
+/** 
+ * @description: http://hanhailong.com/2016/12/10/Synchronized%E4%B8%8ELock%E9%94%81%E7%9A%84%E5%8C%BA%E5%88%AB/
+ * @author viakiba
+ * @date 2017年7月7日
+ */
+
+public class AppTest {
+	public static void test(int round,int threadNum,CyclicBarrier cyclicBarrier){     
+        new TestSync("Sync",round,threadNum,cyclicBarrier).testTime();     
+        new TestLock("Lock",round,threadNum,cyclicBarrier).testTime();     
+        new TestAtomic("Atom",round,threadNum,cyclicBarrier).testTime();     
+    }     
+
+    public static void main(String args[]){     
+        for(int i=0;i<5;i++){     
+            int round=100000*(i+1);     
+            int threadNum=5*(i+1);     
+            CyclicBarrier cb=new CyclicBarrier(threadNum*2+1);     
+            System.out.println("==========================");     
+            System.out.println("round:"+round+" thread:"+threadNum);     
+            test(round,threadNum,cb);     
+
+        }     
+    }  
+}
---
+++ LockVSSynchronized/src/com/vk/test_1/TestSync.java
@@ -0,0 +1,36 @@
+package com.vk.test_1;
+
+import java.util.concurrent.CyclicBarrier;
+
+/** 
+ * @description: 
+ * @author viakiba
+ * @date 2017年7月7日
+ */
+
+public class TestSync extends TestTemplate{
+
+	/**
+	 * @param _id
+	 * @param _round
+	 * @param _threadNum
+	 * @param _cb
+	 */
+	public TestSync(String _id, int _round, int _threadNum, CyclicBarrier _cb) {
+		super(_id, _round, _threadNum, _cb);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override    
+    /**   
+     * synchronized关键字不在方法签名里面，所以不涉及重载问题   
+     */    
+    synchronized long  getValue() {     
+        return super.countValue;     
+    }     
+    @Override    
+    synchronized void  sumValue() {     
+        super.countValue+=preInit[index++%round];     
+    }
+
+}
